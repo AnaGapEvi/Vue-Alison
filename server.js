07000fb03799ab3cfeb8b00c2c0cb7906cf4365f
Anahit Gabrielyan <AnahitGabrielyanEvistep@gmail.com$ git config -l$ git config --global credential.helper cachegit config --global user.email AnahitGabrielyanEvistep@gmail.com>
2022-11-08 16:36:20 +0400

    urls

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
+// const express = require('express');
+// const port = process.env.PORT || 8080;
+//
+// const serveStatic = require("serve-static")
+// const path = require('path');
+//
+// const app = express();
+//
+// app.use('/', serveStatic(path.join(__dirname, '/dist')));
+// app.listen(port);
+// console.log(`server started port: ${port}`);
+
+
 const express = require('express');
-const port = process.env.PORT || 8080;
-
-const serveStatic = require("serve-static")
 const path = require('path');
-
+const history = require('connect-history-api-fallback');
 const app = express();
-
-app.use('/', serveStatic(path.join(__dirname, '/dist')));
-app.listen(port);
-console.log(`server started port: ${port}`);
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+app.use(staticFileMiddleware);
+app.use(history({  disableDotRule: true,  verbose: true}));
+app.use(staticFileMiddleware);
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/dist/index.html'));
+});
+const server = app.listen(process.env.PORT || 3000, function () {
+  const port = server.address().port;
+  console.log("App now running on port", port);
+});
 
 // const express = require('express');
 // const path = require('path');
