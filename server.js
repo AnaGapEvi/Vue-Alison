2f1863b1541930986ac5686b5a881d8f3a3a6a06
Anahit Gabrielyan <AnahitGabrielyanEvistep@gmail.com$ git config -l$ git config --global credential.helper cachegit config --global user.email AnahitGabrielyanEvistep@gmail.com>
2022-11-08 15:36:40 +0400

    make it better

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
 const express = require('express');
 const port = process.env.PORT || 8080;
 
-// const serveStatic = require("serve-static")
+const serveStatic = require("serve-static")
 const path = require('path');
 
 const app = express();
 
-// app.use(express.static(path.join(__dirname + 'dist/static')))
-app.get('/', express.static(path.join(__dirname, 'dist/static')));
-// app.get('/', function (req, res) {
-//   res.render(path.join(__dirname + '/dist/index.html'));
-// });
+app.use('/', serveStatic(path.join(__dirname, 'dist')));
 app.listen(port);
 
-
+//
 // const express = require('express');
-// const port = process.env.PORT || 8080;
+// const path = require('path');
 //
-// const serveStatic = require("serve-static")
-// const path = require('path');`
+// const port = process.env.PORT || 3000;
+// const app = express();
 //
-// app = express();
+// app.use(express.static(path.join(__dirname, 'dist')));
 //
-// app.use(serveStatic(path.join(__dirname, 'dist')));
-// app.listen(port);
+// app.get('*', (_req, res) => {
+//   res.sendFile(path.join(__dirname, 'dist/index.html'));
+// });
+//
+// app.listen(port, (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(`server started port: ${port}`);
+//   }
+// });
