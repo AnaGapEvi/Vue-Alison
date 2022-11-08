d76a1733db5231d7f127d31df78b487965e7e90a
Anahit Gabrielyan <AnahitGabrielyanEvistep@gmail.com$ git config -l$ git config --global credential.helper cachegit config --global user.email AnahitGabrielyanEvistep@gmail.com>
2022-11-08 15:55:38 +0400

    urls

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const port = process.env.PORT || 8080;
-
-const serveStatic = require("serve-static")
-const path = require('path');
-
-const app = express();
-
-app.use('/', serveStatic(path.join(__dirname, 'dist')));
-app.listen(port);
+// const express = require('express');
+// const port = process.env.PORT || 8080;
+//
+// const serveStatic = require("serve-static")
+// const path = require('path');
+//
+// const app = express();
+//
+// app.use('/', serveStatic(path.join(__dirname, 'dist')));
+// app.listen(port);
 
 //
 // const express = require('express');
