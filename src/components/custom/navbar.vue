343937bad5eb8d9e6ffd4c1684272dcd2d408bce
Anahit Gabrielyan <AnahitGabrielyanEvistep@gmail.com$ git config -l$ git config --global credential.helper cachegit config --global user.email AnahitGabrielyanEvistep@gmail.com>
2022-11-17 11:47:56 +0400

    change

--- src/components/custom/navbar.vue
+++ src/components/custom/navbar.vue
@@ -130,7 +130,7 @@ export default {
     registerFunction(form) {
       this.axios.post('/register', form)
         .then((resp) => {
-          // this.axios.defaults.headers.common['Authorization'] = 'Bearer ' + resp.data.token;
+          this.axios.defaults.headers.common['Authorization'] = 'Bearer ' + resp.data.token;
           localStorage.setItem('access_token', resp.data.token);
           this.back()
           this.$router.push({name: "AuthHome"})
