e536899c8d50f7f513f59d3e17a8dacf8666d091
Anahit Gabrielyan <AnahitGabrielyanEvistep@gmail.com$ git config -l$ git config --global credential.helper cachegit config --global user.email AnahitGabrielyanEvistep@gmail.com>
2022-11-17 11:06:36 +0400

    change

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 
 import axios from "axios";
 import VueAxios from 'vue-axios'
+import router from "../router";
+import App from "../App.vue";
 
 axios.defaults.baseURL = process.env.API_URL;
 axios.defaults.headers.common['Access-Control-Allow-Origin']='*';
@@ -10,3 +12,35 @@ axios.defaults.headers.common['Authorization']='Bearer '+localStorage.getItem('a
 // axios.interceptors.request
 // axios.interceptors.response
 Vue.use(VueAxios, axios);
+
+
+this.axios.interceptors.response.use(
+  response => response,
+  error => {
+    // Show the user a 500 error
+    if (status >= 500) {
+      console.log({500:error});
+    }
+
+    // Handle Session Timeouts
+    if (status === 401) {
+      console.log({401:error});
+      app.refreshToken();
+    }
+
+    // Handle Forbidden
+    if (status === 403) {
+      console.log({403:error});
+    }
+
+    return Promise.reject(error)
+  }
+);
+
+new Vue({
+  methods: {
+    refreshToken: function() {
+      console.log('refreshing the token');
+    }
+  }
+})
